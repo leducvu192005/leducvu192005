1dc81f1f5cee7914d38d2cbc6730b280f8038628
leducvu192005 <vule59695@gmail.com>
2025-06-04 15:08:19 +0700

    update

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: AppTheme.darkThemeMode,
 
-      home: const LoginPage(),
+      home: const SignupPage(),
     );
   }
 }
