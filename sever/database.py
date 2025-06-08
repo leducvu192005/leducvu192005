70982ad7c65aaa528535ab01d03ebe81df626564
leducvu192005 <vule59695@gmail.com>
2025-06-08 11:10:48 +0700

    fix xong sever

--- sever/database.py
+++ sever/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://postgres:test1234@localhost:5432/fluttermusicapp'
+DATABASE_URL = 'postgresql://postgres:ldv@localhost:5432/flutterspotifyapp'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
